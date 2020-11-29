a5026815d74d820cc0471eff847451c387304c7d
Nina NJul <nina.dev.web@gmail.com>
2020-11-29 05:49:14 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const hamburgerBtn = document.getElementById('hamburgerBtn');
+const navBar = document.getElementById('navBar');
+
+hamburgerBtn.addEventListener('click', () => {
+  navBar.classList.toggle('open');
+});
